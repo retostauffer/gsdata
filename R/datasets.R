d2da1a7b68e8160c28fcc88837cff625e1b695a1
Reto Stauffer <Reto.Stauffer@uibk.ac.at>
2023-03-14 17:48:46 +0100

    fix: more generic approach for gs_dataset() subsetting/filtering

--- R/datasets.R
+++ R/datasets.R
@@ -5,39 +5,61 @@
 #' The Geosphere (ZAMG) datahub API provides an endpoint to get all
 #' available datasets. This function returns 
 #'
-#' @param type \code{NULL} or character. Allows to filter certain
-#'        data set types (see 'Details').
+#' @param \dots query arguments passed to the API. See 'Details' for 
+#'        more examples and classical use cases.
 #' @param version integer, API version (defaults to 1).
 #' @param config empty list by default; can be a named list to be fowrarded
 #'        to the \code{httr::GET} request if needed.
+#' @param verbose logical, if set \code{TRUE} some more output will be produced.
 #'
 #' @return Returns a \code{data.frame} with all available data types and
 #' API endpoints.
 #'
 #' @details
 #' The API provides an enpoint to get all available data sets which
-#' can be filtered. If \code{type = NULL} all available data sets
-#' will be returned, else a specific type is requested.
+#' can be filtered using the \code{\dots} argument. If nothing is specified,
+#' all available data sets will be returned as a \code{data.frame}.
+#' Filtering for any of the variables contained in this \code{data.frame}
+#' is possible. Classical usecase:
 #'
-#' * Unfiltered: <https://dataset.api.hub.zamg.ac.at/v1/datasets>
-#' * Filtered: <https://dataset.api.hub.zamg.ac.at/v1/datasets?type=station>
-#' * Filtered: <https://dataset.api.hub.zamg.ac.at/v1/datasets?type=timeseries>
-#' * Filtered: <https://dataset.api.hub.zamg.ac.at/v1/datasets?type=grid>
-#' * \dots
+#' Return all data sets where \code{mode == "historical"}:
+#' * \code{gs_datasets(mode = "historical")}
+#'
+#' Return all data sets where \code{type == "grid"}:
+#' * \code{gs_datasets(type = "grid")}
+#'
+#' Return all data sets of \code{type == "timeseries"} where one of the
+#' possible response formats is \code{response_format == "geojson")}:
+#' * \code{gs_datasets(type = "timeseries", response_formats = "geojson")}
+#'
+#' This can of couse also be done using \code{subset()} or other subsetting
+#' methods in \emph{R}, however, when specified as input arguments on the \code{\dots}
+#' argument this job is taken care of by the API. Unknown arguments will be ignored
+#' (e.g., \code{country}), if filtering does not work the API will responde
+#' with an error which will be thrown (e.g., when using \code{type = "spatialdata"})
+#' and a list of allowed options will be shown as returned by the API.
 #'
 #' @importFrom httr GET content status_code
 #' @author Reto Stauffer
 #' @export
-gs_datasets <- function(type = NULL, version = 1L, config = list()) {
-    stopifnot("wrong argument 'type'" = is.null(type) || (is.character(type) && length(type) == 1))
+gs_datasets <- function(..., version = 1L, config = list(), verbose = FALSE) {
+
+    # Parsing 'query' arguments (if any)
+    query <- as.list(match.call(expand.dots = TRUE))[-1]
+    query <- query[!grepl("^(version|config|verbose)$", names(query))]
 
     # Get base URL; performs version sanity check
     baseurl <- gs_baseurl(version)
+    URL     <- paste(baseurl, "datasets", sep = "/")
 
-    args <- if (is.null(type)) list() else list(type = type)
-    URL  <- paste(baseurl, "datasets", sep = "/")
+    # Verbosity
+    if (verbose)
+        message(sprintf("Calling: %s%s%s", URL,
+                        if (!length(query)) "" else "?",
+                        if (!length(query)) "" else paste(paste(names(query), query, sep = "="), collapse = "&")))
 
-    req  <- GET(URL, config = config, query = args)
+    # Requesting data
+    req  <- GET(URL, config = config, query = query)
     if (!status_code(req) == 200) {
         tmp <- try(content(req))
         if (is.list(tmp) && !is.null(tmp$detail[[1]]$msg)) {
@@ -47,8 +69,7 @@ gs_datasets <- function(type = NULL, version = 1L, config = list()) {
         }
     }
     content <- content(req)
-
-    keys <- unique(unlist(lapply(content, names)))
+    keys    <- unique(unlist(lapply(content, names)))
 
     # Splitting up the path to get the information
     tmp <- strsplit(sub("^\\/", "", names(content)), "/")
@@ -65,5 +86,7 @@ gs_datasets <- function(type = NULL, version = 1L, config = list()) {
         kn <- if (k == "type") "data_format" else k # New name to not overwrite 'type'
         res[[kn]] <- unname(sapply(content, fn, k = k))
     }
+
+    # Return data.frame
     return(res)
 }
